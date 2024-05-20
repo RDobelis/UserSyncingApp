7db795a8d17fc1f46e01fa4200678aa7f0545d09
Rihards Dobelis <rihards.dobelis@gmail.com>
2024-05-20 19:51:23 +0300

    Split test projects into logical parts, added tests for UserSyncService

---
+++ UserSyncingApp.ServiceInterface.Tests/Mocks/MockHttpMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserSyncingApp.Tests.Mocks;
+
+public class MockHttpMessageHandler : HttpMessageHandler
+{
+    private HttpResponseMessage _responseMessage;
+
+    public MockHttpMessageHandler(HttpResponseMessage responseMessage)
+    {
+        _responseMessage = responseMessage;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) => 
+        Task.FromResult(_responseMessage);
+}
