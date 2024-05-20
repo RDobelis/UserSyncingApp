615cae7b7fd9a6cebb3180720922dcd787848077
Rihards Dobelis <rihards.dobelis@gmail.com>
2024-05-20 21:05:58 +0300

    Formatting update

--- UserSyncingApp.ServiceStackServices/UserSyncService.cs
+++ UserSyncingApp.ServiceStackServices/UserSyncService.cs
@@ -16,24 +16,28 @@ namespace UserSyncingApp.ServiceStackServices
         public async Task<object> Any(SyncRemoteToLocal request)
         {
             await _userService.SyncRemoteToLocalAsync();
+            
             return new SyncResponse { Result = "Sync from remote to local completed" };
         }
 
         public async Task<object> Any(SyncLocalToRemote request)
         {
             await _userService.SyncLocalToRemoteAsync();
+            
             return new SyncResponse { Result = "Sync from local to remote completed" };
         }
 
         public object Any(UpdateUserEmail request)
         {
             _userService.UpdateUserEmail(request.UserId, request.NewEmail);
+            
             return new UpdateResponse { Result = "User email updated" };
         }
 
         public object Any(DeleteUser request)
         {
             _userService.DeleteUser(request.UserId);
+            
             return new DeleteResponse { Result = "User deleted" };
         }
     }
